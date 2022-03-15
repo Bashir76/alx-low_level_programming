5e80188a20845ff6718e737076a37a228cd3ae2d
Bashir76 <99528741+Bashir76@users.noreply.github.com>
2022-03-15 05:54:01 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 /**
  * main - main block
@@ -11,11 +12,11 @@ int main(void)
 {
 	int i;
 
-for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 	}
 	putchar('\n');
 
-return (0);
+	return (0);
 }
