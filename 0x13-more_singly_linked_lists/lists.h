4479388461130103ccd49b7e808e8cddc9a4fe7c
Bashir S Umar <99528741+Bashir76@users.noreply.github.com>
2022-04-26 11:21:24 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,11 @@
-#ifndef _MORE_SINGLY_LINKED_LISTS
-#define _MORE_SINGLY_LINKED_LISTS
+#ifndef LIST_H
+#define LIST_H
+#define TABLE_SIZE 100000
 
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned long int uli;
 
 /**
  * struct listint_s - singly linked list
@@ -19,6 +21,32 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct entry_t - singly linked list
+ * @key: unsigned long integer
+ * @value: unsigned long integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Hash table
+ */
+typedef struct entry_t
+{
+	unsigned long int key;
+	unsigned long int value; /* The address */
+	struct entry_t *next;
+} entry_t;
+
+/**
+ * struct ht_t - hash table
+ * @entries: Array of entries of the table
+ *
+ * Description: Hash table structure
+ */
+typedef struct ht_t
+{
+	entry_t **entries;
+} ht_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -32,9 +60,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-
-/*Helper functions*/
-int _putchar(char c);
-
-#endif
+#endif /* LIST_H */
