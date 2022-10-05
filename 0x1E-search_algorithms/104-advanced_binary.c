923ca3b31a3fddb6b0cef44d67c3c8e19a195893
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-10-05 16:53:02 +0100

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,21 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * main - Entry point
+ * rec_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * Return: Always EXIT_SUCCESS
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-int main(void)
+int rec_search(int *array, size_t size, int value)
 {
-	int array[] = {
-		0, 1, 2, 5, 5, 6, 6, 7, 8, 9
-	};
-	size_t size = sizeof(array) / sizeof(array[0]);
+	size_t half = size / 2;
+	size_t i;
 
-	printf("Found %d at index: %d\n\n", 8, advanced_binary(array, size, 8));
-	printf("Found %d at index: %d\n\n", 5, advanced_binary(array, size, 5));
-	printf("Found %d at index: %d\n", 999, advanced_binary(array, size, 999));
-	return (EXIT_SUCCESS);
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (rec_search(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
+}
+
+/**
+ * advanced_binary - calls to rec_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	int index;
+
+	index = rec_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
 }
